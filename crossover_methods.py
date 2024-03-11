2e38fbbfd50a72995945225aefea3388ba608ff7
dsadovoy <66562593+dsadovoy@users.noreply.github.com>
2024-03-11 11:37:40 +0300

    Add finalized version

--- crossover_methods.py
+++ crossover_methods.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 def single_point_crossover(parents, crossover_prob):
+    """ 
+   Return child after single point crossover of two parents
+
+   Parameters:
+        parents: list of two parents
+        crossover_prob: probability of crossover
+   Return:
+        Child after single point crossover of two parents
+    """
     if np.random.random()<crossover_prob:
         parent_len=len(parents[0])
         point=np.random.choice(parent_len)
