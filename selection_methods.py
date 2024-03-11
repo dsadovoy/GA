45381757d4daca6e397ba34fc5ce28fd871b6c31
dsadovoy <66562593+dsadovoy@users.noreply.github.com>
2024-03-11 11:38:37 +0300

    Add finalized version

--- selection_methods.py
+++ selection_methods.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 def rank_selection(fitness):
+    """ 
+   Return probabilities of parents selection for mating based on rank selection
+
+   Parameters:
+        fitness: list of parents' fitness
+   Return:
+        List of parents' probabilities
+    """
     pop_size = len(fitness)
     parents_probs = np.zeros(pop_size)
     parents_probs_sorted = np.argsort(fitness)
@@ -9,6 +17,15 @@ def rank_selection(fitness):
     return parents_probs
 
 def tournament_selection(fitness, k=3):
+    """ 
+   Return probabilities of parents selection for mating based on deterministic tournament selection
+
+   Parameters:
+        fitness: list of parents' fitness
+        k: number of competitors
+   Return:
+        List of parents' probabilities
+    """
     pop_size = len(fitness)
     parents_probs = np.zeros(pop_size)
     for i in range(pop_size):
@@ -18,8 +35,3 @@ def tournament_selection(fitness, k=3):
         parents_probs[winner_ind]+=1/pop_size
     return parents_probs
 
-def boltzmann_selection(fitness, temperature):
-    fitness = np.array(fitness)
-    fitness_norm = [f / np.sum(fitness) for f in fitness]
-    parents_probs = np.exp(np.array(fitness_norm)/temperature) / np.sum(np.exp(np.array(fitness_norm)/temperature))
-    return parents_probs
