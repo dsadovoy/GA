d3e032199d2f2e3d3036e2674157540a5c9ff156
dsadovoy <66562593+dsadovoy@users.noreply.github.com>
2024-03-11 11:38:15 +0300

    Add finalized version

--- lunar_lander.py
+++ lunar_lander.py
@@ -1,25 +1,19 @@
 import gym
 import numpy as np
-import random
-import tensorflow as tf 
-from tensorflow.keras import models
-from tensorflow.keras import layers
 from chromosome import Chromosome
 
 class LunarLander:
+    """ 
+   A class that represents Lunar Lander gaming environment.
+
+   Attributes:
+        env (object): OpenAI gym Lunar Lander gaming environment
+   Methods:
+        get_score(self, flat_weights): get total score per episode in gaming environment
+    """
     def __init__(self):
         
         self.env = gym.make("LunarLanderContinuous-v2")
-        self.seed_count = 0
-        # self.seed(self.seed_count)
-
-
-    def seed(self, seed_int=0):
-        random.seed(seed_int)
-        np.random.seed(seed_int)
-        self.env.seed(seed_int)
-        self.env.action_space.seed(seed_int)
-        self.seed_count+=1
 
     def get_score(self, flat_weights):
         chromosome = Chromosome()
@@ -29,7 +23,6 @@ class LunarLander:
         weights = chromosome.restore_weights(flat_weights)
         model.set_weights(weights)
         
-        # self.seed(self.seed_count)
         observation = self.env.reset()
           
         while not done:
